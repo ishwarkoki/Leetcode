3b5b9caf42f5d56c1907fe076be34628bea92020
Ishwar kokkili <56781308+ishwarkoki@users.noreply.github.com>
2023-09-01 15:57:47 +0530

    Time: 58 ms (98.87%) | Memory: 23.1 MB (23.28%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,14 +1,25 @@
 class Solution:
     def countBits(self, n: int) -> List[int]: 
-        ans =  []
+        ans =  [None] * (n+1) 
 
-        def count_ones(num): 
-            return bin(num).count('1') 
+        ans[0] = 0 
 
         for num in range(n+1): 
-            ans.append(count_ones(num)) 
 
-        return ans
+            if num%2 == 0 : 
+                ans[num] = ans[num//2] 
+            else : 
+                ans[num] = ans[num//2]+1 
+
+        return ans 
+
+# A number is expressed as : 
+# half_number<<1 if number is even  
+# (half_number<<1) +1 if number is odd
+# And we know that left shift will add just a zero at end, so same number of bits as half_number if even and add one more bit if odd
+ 
+
+        
 
 
         
