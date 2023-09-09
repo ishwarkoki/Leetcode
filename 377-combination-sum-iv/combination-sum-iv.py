4316fa972efb23f29b003efa0a24f17bade985ed
Ishwar kokkili <56781308+ishwarkoki@users.noreply.github.com>
2023-09-09 12:39:17 +0530

    Time: 66 ms (7.27%) | Memory: 18.6 MB (6.40%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.py
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -2,27 +2,26 @@ class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int: 
 
         n = len(nums) 
-        dp = {} 
+        dp = [[-1 for _ in range(target+1)] for i in range(len(nums)+1) ] 
 
         def solve(i, t): 
 
-            if i >= n : 
+            if i >= n or t > target : 
                 return 0 
 
             if t == target : 
                 return 1 
 
-            if t > target : 
-                return 0  
+            
 
-            if (i, t) in dp : 
-                return dp[(i,t)]
+            if dp[i][t] != -1 : 
+                return dp[i][t]
 
             take = solve(0,t+nums[i]) 
             skip = solve(i+1,t) 
 
-            dp[(i, t)] = take + skip 
-            return dp[(i, t)]
+            dp[i][t] = take + skip 
+            return dp[i][t]
 
         return solve(0, 0)
 
