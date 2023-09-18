b0635d1d10c6d5e5603fd900211cc2b5038a69d3
Ishwar kokkili <56781308+ishwarkoki@users.noreply.github.com>
2023-09-18 12:48:52 +0530

    Time: 108 ms (48.84%) | Memory: 16.6 MB (94.25%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.py
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.py
@@ -1,17 +1,36 @@
 class Solution:
+    def bs(self, nums): 
+        lo, hi = 0, len(nums)-1 
+        res = -1 
+
+        while lo<=hi : 
+            mid = lo + (hi-lo)//2 
+
+            if nums[mid] == 1 : 
+                res = mid 
+                lo = mid + 1 
+
+            else : 
+                hi = mid-1 
+
+        return 0 if res == -1 else res+1 
+
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:  
 
         min_heap = []
         ans = []
 
         for i in range(len(mat)): 
-            heapq.heappush(min_heap,(sum(mat[i]),i)) 
+            heapq.heappush(min_heap,(self.bs(mat[i]),i))  
 
         for _ in range(k): 
             sum_, idx = heapq.heappop(min_heap)
             ans.append(idx) 
 
-        return ans
+        return ans  
+
+
+        # n * log(n)
 
 
 
