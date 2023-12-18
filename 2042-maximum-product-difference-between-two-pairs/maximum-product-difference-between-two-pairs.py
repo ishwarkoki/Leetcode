35f7766a36a01212ac1c118926da9280d665c088
Ishwar kokkili <56781308+ishwarkoki@users.noreply.github.com>
2023-12-18 12:40:31 +0530

    Time: 161 ms (32.72%) | Memory: 17.8 MB (35.35%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.py
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.py
@@ -1,6 +1,24 @@
 class Solution:
     def maxProductDifference(self, nums: List[int]) -> int: 
 
-        nums.sort()  
+        max1, max2, min1, min2 = 0, 0, 10**4 + 1, 10**4 + 1
 
-        return (nums[-1]*nums[-2]) - (nums[0]*nums[1])
+        for num in nums: 
+            if num > max1 : 
+                max2 = max1
+                max1 = num
+
+            elif num > max2 : 
+                max2 = num 
+                 
+            if num < min1 : 
+                min2 = min1
+                min1 = num 
+            
+            elif num < min2 : 
+                min2 = num
+                
+        print(max2, max1, min2, min1)
+        return max2*max1 - min2*min1 
+
+
