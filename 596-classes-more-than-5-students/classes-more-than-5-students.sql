e638221c21c4296bdf61f368931fb9cfe3eb9ee1
Ishwar kokkili <56781308+ishwarkoki@users.noreply.github.com>
2023-12-18 21:00:17 +0530

    Time: 1726 ms (17.45%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-more-than-5-students/classes-more-than-5-students.sql
+++ 596-classes-more-than-5-students/classes-more-than-5-students.sql
@@ -1,11 +1,12 @@
 /* Write your T-SQL query statement below */
 
-with course_cte as (
-    select class, count(class) number_of_students
-    from Courses 
-    group by class 
-)
+-- with course_cte as (
+--     select class, count(class) number_of_students
+--     from Courses 
+--     group by class 
+-- )
 
-select class 
-from course_cte 
-where number_of_students >=5 
+select class
+from Courses 
+group by class 
+having count(class) >= 5 
